43ad2205a9ebab7b3cfa6e6774f8fcf0d4caba8c
Rene Espinoza <recespinoza@gmail.com>
2022-04-07 00:22:55 -0400

    challenge 2

---
+++ chapters/ch2/challenge2.py
@@ -0,0 +1,4 @@
+season = input('Enter season: ')
+number = input('Enter number: ')
+adjective = input('Enter adjective')
+print(f'On a {adjective} {season} day, I drink at least {number} cups of juice! ')
